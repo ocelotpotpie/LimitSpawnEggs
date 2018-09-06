514078f94a0989b77515909ccbf61f6b1271db5a
totemo <TotemoResearch@gmail.com>
2018-09-06 23:33:43 +0930

    Version 1.0.0
     * Added a bypass permission so admins can configure spawners with spawn eggs.
     * Made the list of blocked spawn eggs configurable so new spawn eggs can be
       supported without code changes.
     * Added a configuration reload command.
     * Message players when a spawn egg is blocked.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # LimitSpawnEggs
+====================
 Prevent spawn eggs from changing spawners.
+
+
+Commands
+--------
+ * `/limitspawneggs [help|reload]` - Show help on the `/limitspawneggs` 
+   command or reload the configuration.
+
+
+Permissions
+-----------
+ * `limitspawneggs.bypass` - Permission to bypass restrictions on the use of
+   spawn eggs on mob spawners.
+ * `limitspawneggs.console` - Permission to run `/limitspawneggs` and
+   thereby reload the configuration (logically requires console access to edit
+   the config).
+
+
+Configuration
+-------------
+ * `egg-types` - List of material names of spawn eggs that are blocked. Allows
+   for future versions of Minecraft to add new mobs without requiring code
+   changes.
--- src/nu/nerd/limitspawneggs/LimitSpawnEggs.java
+++ src/nu/nerd/limitspawneggs/LimitSpawnEggs.java
@@ -1,7 +1,13 @@
 package nu.nerd.limitspawneggs;
 
+import java.util.EnumSet;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -25,20 +31,74 @@ public class LimitSpawnEggs extends JavaPlugin implements Listener {
      */
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        reloadConfig();
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
     // ------------------------------------------------------------------------
     /**
-     * Prevent any player from right-clicking on a mob spawner with a spawn egg.
+     * @see org.bukkit.plugin.java.JavaPlugin#onCommand(org.bukkit.command.CommandSender,
+     *      org.bukkit.command.Command, java.lang.String, java.lang.String[])
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+        if (command.getName().equalsIgnoreCase(getName())) {
+            if (args.length == 1) {
+                if (args[0].equalsIgnoreCase("help")) {
+                    return false;
+                } else if (args[0].equalsIgnoreCase("reload")) {
+                    reloadConfig();
+                    sender.sendMessage(ChatColor.GOLD + getName() + " configuration reloaded.");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // ------------------------------------------------------------------------
+    /**
+     * Prevent any player from right-clicking on a mob spawner with a spawn egg,
+     * unless they have the limitspawneggs.bypass permission.
      */
     @EventHandler(ignoreCancelled = true)
     public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (player.hasPermission("limitspawneggs.bypass")) {
+            return;
+        }
+
         if (event.getAction() == Action.RIGHT_CLICK_BLOCK &&
-            event.getClickedBlock().getType() == Material.MOB_SPAWNER &&
+            event.getClickedBlock().getType() == Material.SPAWNER &&
             event.getItem() != null &&
-            event.getItem().getType() == Material.MONSTER_EGG) {
+            _spawnEggs.contains(event.getItem().getType())) {
             event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "You don't have permission to alter spawners.");
         }
     }
+
+    // ------------------------------------------------------------------------
+    /**
+     * Load the list of spawn egg materials to block.
+     */
+    @Override
+    public void reloadConfig() {
+        super.reloadConfig();
+        _spawnEggs.clear();
+        for (String eggTypeName : getConfig().getStringList("egg-types")) {
+            try {
+                _spawnEggs.add(Material.valueOf(eggTypeName));
+            } catch (IllegalArgumentException ex) {
+                getLogger().warning("\"" + eggTypeName + "\" is not a valid material name.");
+            }
+        }
+    }
+
+    // ------------------------------------------------------------------------
+    /**
+     * Set of Materials representing spawn eggs that are blocked.
+     */
+    private final EnumSet<Material> _spawnEggs = EnumSet.noneOf(Material.class);
+
 } // class LimitSpawnEggs
